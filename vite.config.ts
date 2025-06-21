9456b83720485901310763fb6f9341e23bb79566
Kroxitrock <kroxitrock@gmail.com>
2025-06-21 15:40:53 +0300

    Add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-
+    base: '/gm-spotlight-app/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
